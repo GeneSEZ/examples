f55e8d1fc0ecfc5de02a2f616feac8e25914289c
Nico Herbig <nico@nicoherbig.io>
2012-02-10 23:05:55 +0000

    change generation date of files from 2011 to 2012

--- de.genesez.example.typo3v4.mvc.GenesezInventory/Classes/Domain/Model/Product.php
+++ de.genesez.example.typo3v4.mvc.GenesezInventory/Classes/Domain/Model/Product.php
@@ -6,7 +6,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2011 Nico Herbig, Jochen Rau
+ *  (c) 2012 Nico Herbig, Jochen Rau
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
