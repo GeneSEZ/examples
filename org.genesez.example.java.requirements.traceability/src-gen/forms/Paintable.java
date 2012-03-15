b30c0ec9065152f269ed6976cb5e11dd3804a94b
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2012-03-15 00:00:37 +0000

    added java requirements traceability example project

---
+++ org.genesez.example.java.requirements.traceability/src-gen/forms/Paintable.java
@@ -0,0 +1,28 @@
+package forms;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_16_0_b6f02e1_1249464729937_936506_1005) 
+ */
+
+import java.awt.Color;
+
+/**
+ * describes <b>paintable</b> things
+ * @author dreamer
+ */
+public interface Paintable {
+	
+	/**
+	 * changes the color by repainting using the specified color
+	 * @param	color	the color to use for repainting
+	 */
+	public void repaint(Color color);
+	
+	// -- generated code of other cartridges --------------------------------
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(java.interface.own.code.declaration._16_0_b6f02e1_1249464729937_936506_1005) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
+}
