8ba2152872232b77bd7c3d4f04ae9e225e7a0896
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2012-03-15 00:00:37 +0000

    added java requirements traceability example project

---
+++ examples/trunk/org.genesez.example.java.requirements.traceability/src-gen/forms/Moveable.java
@@ -0,0 +1,37 @@
+package forms;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_14_0_b6f02e1_1214478843156_556754_163) 
+ */
+
+import static traceability.Requirement.Value.R5;
+import traceability.Requirement;
+
+/**
+ * describes things which can be <i><b>moved</b></i>
+ * @author dreamer
+ */
+@Requirement({
+	R5
+})
+public interface Moveable {
+	
+	/**
+	 * moves the position by the given delta values within a 2-dimensional 
+	 * coordinate system
+	 * @param	dx	delta value to move in <i><b>x</b></i> direction
+	 * @param	dy	delta value to move in <i><b>y</b></i> direction
+	 */
+	@Requirement({
+		R5
+	})
+	public void move(double dx, double dy);
+	
+	// -- generated code of other cartridges --------------------------------
+	
+	// -- own code implementation -------------------------------------------
+	/* PROTECTED REGION ID(java.interface.own.code.declaration._14_0_b6f02e1_1214478843156_556754_163) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
+}
