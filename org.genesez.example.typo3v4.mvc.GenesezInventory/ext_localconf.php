accbfe8b1617b7cbf09c931a83bbc699a3e857d3
Nico Herbig <nico@nicoherbig.io>
2012-08-13 22:46:16 +0000

    update inventory example to work with the actual TYPO3 platform

--- org.genesez.example.typo3v4.mvc.GenesezInventory/ext_localconf.php
+++ org.genesez.example.typo3v4.mvc.GenesezInventory/ext_localconf.php
@@ -15,6 +15,11 @@ Tx_Extbase_Utility_Extension::configurePlugin(
 	Tx_Extbase_Utility_Extension::PLUGIN_TYPE_PLUGIN
 );
 
+if (TYPO3_MODE === 'BE') {
+	/* PROTECTED REGION ID(ext_localconf.php.own.command.controller.code.implementation.eee_1045467100313_135436_1) ENABLED START */
+	/* PROTECTED REGION END */
+}
+
 /* PROTECTED REGION ID(ext_localconf.php.own.code.implementation.eee_1045467100313_135436_1) ENABLED START */
 /* PROTECTED REGION END */
 
