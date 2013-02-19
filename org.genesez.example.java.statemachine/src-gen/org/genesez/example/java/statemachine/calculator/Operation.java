6cbf1201e5f2e243467c0ff9bf72c3a90d7cc4e1
toh <toh@a19731f8-c322-0410-8b9b-f98442d78366>
2013-02-19 16:37:22 +0000

    revised state machine example to work with current genesez development, added impl again, renamed it, made unit tests running, refs #155

---
+++ org.genesez.example.java.statemachine/src-gen/org/genesez/example/java/statemachine/calculator/Operation.java
@@ -0,0 +1,18 @@
+package org.genesez.example.java.statemachine.calculator;
+
+/* 
+ *	Do not place import/include statements above this comment, just below. 
+ * 	@FILE-ID : (_15_5_1_6340215_1232981000430_912204_1054) 
+ */
+
+/**
+ * Please describe the responsibility of your class in your modeling tool.
+ */
+public enum Operation {
+	
+	ADD, SUB, MUL, DIV, POW, NOP;
+	
+	/* PROTECTED REGION ID(java.enumeration.own.code.implementation._15_5_1_6340215_1232981000430_912204_1054) ENABLED START */
+	// TODO: put your own implementation code here
+	/* PROTECTED REGION END */
+}
