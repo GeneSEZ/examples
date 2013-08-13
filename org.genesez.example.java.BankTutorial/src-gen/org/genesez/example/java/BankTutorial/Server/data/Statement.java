69bac78d635cf66aacbaa86f20ad0508ab6a10ef
andpfl <andpfl@a19731f8-c322-0410-8b9b-f98442d78366>
2013-08-13 08:10:15 +0000

    delete unnecessary files in bank tutorial example

--- org.genesez.example.java.BankTutorial/src-gen/org/genesez/example/java/BankTutorial/Server/data/Statement.java
+++ org.genesez.example.java.BankTutorial/src-gen/org/genesez/example/java/BankTutorial/Server/data/Statement.java
@@ -6,12 +6,14 @@ package org.genesez.example.java.BankTutorial.Server.data;
  */
 import java.io.Serializable;
 import java.util.Date;
+
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Version;
-import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Version;
 
 /**
  * A statement logs all operations of an account.
