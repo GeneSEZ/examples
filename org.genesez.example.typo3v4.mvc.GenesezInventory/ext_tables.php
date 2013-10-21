20b958d1048679808a6ead110ecd364147518c94
Nico Herbig <nico@nicoherbig.io>
2013-10-21 16:19:05 +0000

    [TASK] Rename protected region.

--- org.genesez.example.typo3v4.mvc.GenesezInventory/ext_tables.php
+++ org.genesez.example.typo3v4.mvc.GenesezInventory/ext_tables.php
@@ -10,23 +10,27 @@ if (!defined ('TYPO3_MODE')) {
 	'List'
 );
 
-/* PROTECTED REGION ID(ext_tables.php.own.plugin.code.implementation.eee_1045467100313_135436_1) ENABLED START */
+/* PROTECTED REGION ID(ext_tables.php.own.plugin.code.eee_1045467100313_135436_1) ENABLED START */
+// TODO: put your further plugin code here
 /* PROTECTED REGION END */
 
 if (TYPO3_MODE === 'BE') {
-	/* PROTECTED REGION ID(ext_tables.php.own.module.code.implementation.eee_1045467100313_135436_1) ENABLED START */
+	/* PROTECTED REGION ID(ext_tables.php.own.module.code.eee_1045467100313_135436_1) ENABLED START */
+	// TODO: put your further module code here
 	/* PROTECTED REGION END */
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'GeneSEZ GszInventory Setup');
 
-/* PROTECTED REGION ID(ext_tables.php.own.typoscript.code.implementation.eee_1045467100313_135436_1) ENABLED START */
+/* PROTECTED REGION ID(ext_tables.php.own.typoscript.code.eee_1045467100313_135436_1) ENABLED START */
+// TODO: put your further TypoScript code here
 /* PROTECTED REGION END */
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_gszinventory_domain_model_product', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/.locallang_csh_tx_gszinventory_domain_model_product.xlf');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_gszinventory_domain_model_product', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_tx_gszinventory_domain_model_product.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_gszinventory_domain_model_product');
 	
-/* PROTECTED REGION ID(ext_tables.php.own.code.implementation.eee_1045467100313_135436_1) ENABLED START */
+/* PROTECTED REGION ID(ext_tables.php.own.code.eee_1045467100313_135436_1) ENABLED START */
+// TODO: put your further code here
 /* PROTECTED REGION END */
 
 ?>
