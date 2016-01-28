dabf07bd51f3be83e31bfe817cd3ff37fc08b189
nihe <nihe@a19731f8-c322-0410-8b9b-f98442d78366>
2016-01-28 12:17:00 +0000

    [TASK] Make TYPO3 CMS extension gsz_inventory TYPO3 CMS 7 compatible

--- examples/trunk/org.genesez.example.typo3v4.mvc.GenesezInventory/ext_tables.php
+++ examples/trunk/org.genesez.example.typo3v4.mvc.GenesezInventory/ext_tables.php
@@ -1,13 +1,11 @@
 <?php
 
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
-}
+defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'List',
-	'List'
+    $_EXTKEY,
+    'List',
+    'List'
 );
 
 /* PROTECTED REGION ID(ext_tables.php.own.plugin.code.eee_1045467100313_135436_1) ENABLED START */
@@ -15,9 +13,9 @@ if (!defined ('TYPO3_MODE')) {
 /* PROTECTED REGION END */
 
 if (TYPO3_MODE === 'BE') {
-	/* PROTECTED REGION ID(ext_tables.php.own.module.code.eee_1045467100313_135436_1) ENABLED START */
-	// TODO: put your further module code here
-	/* PROTECTED REGION END */
+    /* PROTECTED REGION ID(ext_tables.php.own.module.code.eee_1045467100313_135436_1) ENABLED START */
+    // TODO: put your further module code here
+    /* PROTECTED REGION END */
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'GeneSEZ GszInventory Setup');
@@ -26,11 +24,8 @@ if (TYPO3_MODE === 'BE') {
 // TODO: put your further TypoScript code here
 /* PROTECTED REGION END */
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_gszinventory_domain_model_product');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_gszinventory_domain_model_product', 'EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_csh_tx_gszinventory_domain_model_product.xlf');
-	
+
 /* PROTECTED REGION ID(ext_tables.php.own.code.eee_1045467100313_135436_1) ENABLED START */
 // TODO: put your further code here
 /* PROTECTED REGION END */
-
-?>
